424a3556a24a6ace3aac1ad398fa68f27757fc18
frankma <guang.y.ma@gmail.com>
2018-05-24 23:00:55 -0400

    add constructor to csrand

--- math/src/cs_random.py
+++ math/src/cs_random.py
@@ -1,3 +1,28 @@
+import numpy as np
+
+
 class CSRandom(object):
+    BIG_INT = 2147483647
+    Z = 0
+    M_SEED = 161803398
+
     def __init__(self, seed: int):
         self.seed = seed
+        self.seed_array = self.__gen_seed_array()
+
+    def __gen_seed_array(self):
+        arr = np.zeros(55, dtype=int)
+        arr[0], arr[1] = self.M_SEED - self.seed, 1
+        for idx in range(2, 55):
+            arr[idx] = self.__bound_int(arr[idx - 2] - arr[idx - 1])
+        arr = arr[::-1]
+        shuffle_order = np.array([(21 * x) % 55 - 1 for x in range(54, -1, -1)], dtype=int)
+        arr = arr[shuffle_order]
+        shuffle_order = [(x + 31) % 55 for x in range(55)]
+        for _ in range(4):
+            for idx in range(55):
+                arr[idx] = self.__bound_int(arr[idx] - arr[shuffle_order[idx]])
+        return arr
+
+    def __bound_int(self, value: int):
+        return value if value > 0 else value + self.M_SEED
--- math/tests/test_cs_random.py
+++ math/tests/test_cs_random.py
@@ -1,5 +1,10 @@
 from unittest import TestCase
 
+from math.src.cs_random import CSRandom
+
 
 class TestCSRandom(TestCase):
+    seed = 99999
+    rand = CSRandom(seed)
+    print(rand.seed_array)
     pass
