62274526c07fb909b4b962aef788468efcd7baaa
frankma <guang.y.ma@gmail.com>
2018-06-13 06:50:19 -0400

    add scenario class

---
+++ core/src/scenario.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class Scenario(object):
+    def __init__(self, scenarios: np.ndarray):
+        self._scenarios = scenarios
+        pass
+
+    def calculate_shock(self):
+        pass
+
+    pass
---
+++ core/test/test_scenario.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class TestScenario(TestCase):
+    def test_calculate_shock(self):
+        pass
+
+    pass
