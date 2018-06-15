5516cbfc6a5aed941259387240ed7e1b89f2e845
frankma <guang.y.ma@gmail.com>
2018-06-15 07:13:58 -0400

    minor update

--- riskfactor/test/test_RFETCopula.py
+++ riskfactor/test/test_RFETCopula.py
@@ -57,7 +57,7 @@ class TestRFETCopula(TestCase):
         mu_2 = 0.01
         sig_2 = 0.45
 
-        rho = 0.1
+        rho = 0.5
 
         time_series_length = 1000.0
         time_series_sample_points = 10 ** 6
@@ -68,7 +68,68 @@ class TestRFETCopula(TestCase):
         norm_rand_2 = norm_rand_1 * rho + np.sqrt(1.0 - rho ** 2) * norm_rand_aux
 
         correl = np.corrcoef(norm_rand_1, norm_rand_2)
-        print(correl)
+
+        print(rho, correl[0][1])
+
+        drift_1 = (mu_1 - 0.5 * sig_1 * sig_1) * np.ones(time_series_sample_points) * dt
+        diffusion_1 = sig_1 * norm_rand_1 * np.sqrt(dt)
+        dlnS_1 = drift_1 + diffusion_1
+        s_time_series_1 = spot_1 * np.exp(np.cumsum(dlnS_1))
+
+        drift_2 = (mu_2 - 0.5 * sig_2 * sig_2) * np.ones(time_series_sample_points) * dt
+        diffusion_2 = sig_2 * norm_rand_2 * np.sqrt(dt)
+        dlnS_2 = drift_2 + diffusion_2
+        s_time_series_2 = spot_2 * np.exp(np.cumsum(dlnS_2))
+
+        return_time_series_1 = s_time_series_1[1:] / s_time_series_1[:-1] - 1.0
+        mu_1_hat = np.average(return_time_series_1) / dt
+        sig_1_hat = np.std(return_time_series_1) / np.sqrt(dt)
+
+        print(mu_1, mu_1_hat)
+        print(sig_1, sig_1_hat)
+
+        return_time_series_2 = s_time_series_2[1:] / s_time_series_2[:-1] - 1.0
+        mu_2_hat = np.average(return_time_series_2) / dt
+        sig_2_hat = np.std(return_time_series_2) / np.sqrt(dt)
+
+        print(mu_2, mu_2_hat)
+        print(sig_2, sig_2_hat)
+
+        correl_hat = np.corrcoef(return_time_series_1, return_time_series_2)
+
+        print(rho, correl_hat[0][1])
+
+        num_path = 10 ** 4
+
+        rfe_1 = RFETCopula(num_path=num_path, co_dep_data_size=time_series_sample_points, co_dep_data_shift=0,
+                         decay_rate=1.0, left_percentile=0.01, right_percentile=99.99)
+        ret_time_series_vol_adj_1 = rfe_1.adjust_time_series_volatility(return_time_series_1)
+        marginal_dist_1 = rfe_1.generate_marginal_dist(ret_time_series_vol_adj_1)
+        shuffle_1 = rfe_1.generate_shuffle_indices(s_time_series_1)
+        scenarios_1 = marginal_dist_1[shuffle_1]
+
+        mu_1_sim = np.average(scenarios_1) / dt
+        sig_1_sim = np.std(scenarios_1) / np.sqrt(dt)
+
+        print(mu_1, mu_1_sim)
+        print(sig_1, sig_1_sim)
+
+        rfe_2 = RFETCopula(num_path=num_path, co_dep_data_size=time_series_sample_points, co_dep_data_shift=0,
+                         decay_rate=1.0, left_percentile=0.01, right_percentile=99.99)
+        ret_time_series_vol_adj_2 = rfe_2.adjust_time_series_volatility(return_time_series_2)
+        marginal_dist_2 = rfe_2.generate_marginal_dist(ret_time_series_vol_adj_2)
+        shuffle_2 = rfe_2.generate_shuffle_indices(s_time_series_2)
+        scenarios_2 = marginal_dist_2[shuffle_2]
+
+        mu_2_sim = np.average(scenarios_2) / dt
+        sig_2_sim = np.std(scenarios_2) / np.sqrt(dt)
+
+        print(mu_2, mu_2_sim)
+        print(sig_2, sig_2_sim)
+
+        correl_sim = np.corrcoef(scenarios_1, scenarios_2)
+
+        print(rho, correl_sim[0][1])
 
         pass
 
