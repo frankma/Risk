a5ca6afffdb86ab79a5ed0e5ae40926d95a1d6ea
Frank Ma <guang.y.ma@gmail.com>
2018-06-25 08:51:12 -0400

    add assert equal to test

--- riskengine/test/test_RiskEngineTCopula.py
+++ riskengine/test/test_RiskEngineTCopula.py
@@ -29,9 +29,6 @@ class TestRiskEngineTCopula(TestCase):
         mu_hat = np.average(return_time_series) / dt
         sig_hat = np.std(return_time_series) / np.sqrt(dt)
 
-        print(mu, mu_hat)
-        print(sig, sig_hat)
-
         num_path = 10 ** 4
         rfe = RiskEngineTCopula(num_path=num_path, co_dep_data_size=time_series_sample_points, co_dep_data_shift=0,
                                 decay_rate=1.0, left_percentile=0.01, right_percentile=99.99)
@@ -43,8 +40,12 @@ class TestRiskEngineTCopula(TestCase):
         mu_sim = np.average(scenarios) / dt
         sig_sim = np.std(scenarios) / np.sqrt(dt)
 
-        print(mu, mu_sim)
-        print(sig, sig_sim)
+        print('\n\tin\that\tsim\tsim_vs_hat')
+        print('mu \t%.4e\t%.4e\t%.4e\t%.4e' % (mu, mu_hat, mu_sim, mu_hat - mu_sim))
+        print('sig\t%.4e\t%.4e\t%.4e\t%.4e' % (sig, sig_hat, sig_sim, sig_hat - sig_sim))
+
+        self.assertAlmostEqual(mu_hat, mu_sim, delta=1e-4)
+        self.assertAlmostEqual(sig_hat, sig_sim, delta=1e-4)
 
         pass
 
@@ -67,9 +68,7 @@ class TestRiskEngineTCopula(TestCase):
         norm_rand_aux = np.random.normal(size=time_series_sample_points)
         norm_rand_2 = norm_rand_1 * rho + np.sqrt(1.0 - rho ** 2) * norm_rand_aux
 
-        correl = np.corrcoef(norm_rand_1, norm_rand_2)
-
-        print(rho, correl[0][1])
+        correl = np.corrcoef(norm_rand_1, norm_rand_2)[0][1]
 
         drift_1 = (mu_1 - 0.5 * sig_1 * sig_1) * np.ones(time_series_sample_points) * dt
         diffusion_1 = sig_1 * norm_rand_1 * np.sqrt(dt)
@@ -85,21 +84,11 @@ class TestRiskEngineTCopula(TestCase):
         mu_1_hat = np.average(return_time_series_1) / dt
         sig_1_hat = np.std(return_time_series_1) / np.sqrt(dt)
 
-        print(mu_1, mu_1_hat)
-        print(sig_1, sig_1_hat)
-
         return_time_series_2 = s_time_series_2[1:] / s_time_series_2[:-1] - 1.0
         mu_2_hat = np.average(return_time_series_2) / dt
         sig_2_hat = np.std(return_time_series_2) / np.sqrt(dt)
 
-        print(mu_2, mu_2_hat)
-        print(sig_2, sig_2_hat)
-
-        correl_hat = np.corrcoef(return_time_series_1, return_time_series_2)
-
-        print(rho, correl_hat[0][1])
-
-        num_path = 10 ** 6
+        num_path = 10 ** 4
 
         rfe = RiskEngineTCopula(num_path=num_path, co_dep_data_size=time_series_sample_points - 1, co_dep_data_shift=0,
                                 decay_rate=1.0, left_percentile=0.01, right_percentile=99.99)
@@ -112,9 +101,6 @@ class TestRiskEngineTCopula(TestCase):
         mu_1_sim = np.average(scenarios_1) / dt
         sig_1_sim = np.std(scenarios_1) / np.sqrt(dt)
 
-        print(mu_1, mu_1_sim)
-        print(sig_1, sig_1_sim)
-
         ret_time_series_vol_adj_2 = rfe.adjust_time_series_volatility(return_time_series_2)
         marginal_dist_2 = rfe.generate_marginal_dist(ret_time_series_vol_adj_2)
         shuffle_2 = rfe.generate_shuffle_indices(ret_time_series_vol_adj_2)
@@ -123,13 +109,21 @@ class TestRiskEngineTCopula(TestCase):
         mu_2_sim = np.average(scenarios_2) / dt
         sig_2_sim = np.std(scenarios_2) / np.sqrt(dt)
 
-        print(mu_2, mu_2_sim)
-        print(sig_2, sig_2_sim)
+        cor_sim_in = np.corrcoef(return_time_series_1, return_time_series_2)[0][1]
+        cor_sim_out = np.corrcoef(scenarios_1, scenarios_2)[0][1]
 
-        cor_sim_in = np.corrcoef(return_time_series_1, return_time_series_2)
-        cor_sim_out = np.corrcoef(scenarios_1, scenarios_2)
+        print('\n\tin\that\tsim\tsim_vs_hat')
+        print('mu_1\t%.4e\t%.4e\t%.4e\t%.4e' % (mu_1, mu_1_hat, mu_1_sim, mu_1_hat - mu_1_sim))
+        print('sig_1\t%.4e\t%.4e\t%.4e\t%.4e' % (sig_1, sig_1_hat, sig_1_sim, sig_1_hat - sig_1_sim))
+        print('mu_2\t%.4e\t%.4e\t%.4e\t%.4e' % (mu_2, mu_2_hat, mu_2_sim, mu_2_hat - mu_2_sim))
+        print('sig_2\t%.4e\t%.4e\t%.4e\t%.4e' % (sig_2, sig_2_hat, sig_2_sim, sig_2_hat - sig_2_sim))
+        print('rho\t%.4e\t%.4e\t%.4e\t%.4e' % (rho, cor_sim_in, cor_sim_out, cor_sim_in - cor_sim_out))
 
-        print(rho, cor_sim_in[0][1], cor_sim_out[0][1])
+        self.assertAlmostEqual(mu_1_hat, mu_1_sim, delta=1e-4)
+        self.assertAlmostEqual(sig_1_hat, sig_1_sim, delta=1e-4)
+        self.assertAlmostEqual(mu_2_hat, mu_2_sim, delta=1e-4)
+        self.assertAlmostEqual(sig_2_hat, sig_2_sim, delta=1e-4)
+        self.assertAlmostEqual(cor_sim_in, cor_sim_out, delta=1e-2)
 
         pass
 
