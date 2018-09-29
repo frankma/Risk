042cac9c4549a57caf1f234fee13a2527d45cfda
frankma <guang.y.ma@gmail.com>
2018-09-28 23:22:35 -0400

    rework node and tree

--- hierarchy/src/node.py
+++ hierarchy/src/node.py
@@ -1,27 +1,11 @@
-from typing import List
-
-
 class Node(object):
-    JOINER_NAME = "#^#"
-    JOINER_TAGS = "."
+    JOINER_NAME = "^"
 
-    def __init__(self, name: str, tags: List, parent, children: List):
+    def __init__(self, name: str, parent=None, children: dict = None):
         self._name = name
-        self._tags = tags
         self._parent = parent
-        self._children = children
-        self._next_child_seq = children.__len__()  # allow bypassing for possible prune of children
+        self._children = children if children is not None else dict()
         pass
-
-    def get_node_level(self):
-        return self._tags.__len__()
-
-    def get_tags(self):
-        return self._tags
-
-    def get_tags_string(self):
-        tags_string = ["%s" % x for x in self._tags]
-        return Node.JOINER_TAGS.join(tags_string)
 
     def get_parent(self):
         return self._parent
@@ -34,7 +18,7 @@ class Node(object):
 
     def get_name_full(self):
         name_full = self.get_name()
-        if self.is_root():
+        if not self.is_root():
             name_full = self.get_parent().get_name_full() + Node.JOINER_NAME + name_full
         return name_full
 
@@ -45,47 +29,19 @@ class Node(object):
             return self.get_parent().get_root()
         pass
 
-    def __hash__(self):
-        return hash(self.get_tags_string())
-
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
-
     def create_child(self, child_name: str):
-        child_tags = self._tags.copy()
-        child_tags.append(self._next_child_seq)
-        child = Node(child_name, child_tags, self, [])
-        self._children.append(child)
-        self._next_child_seq += 1
+        child = Node(child_name, self, dict())
+        self._children[child.get_name()] = child
         return child
 
-    def add_child(self, node):
-        if self.is_my_child(node) and not self.is_child_added(node):
-            self._children.append(node)
-            self._next_child_seq = max(node.get_tags()[-1], self._next_child_seq) + 1
-        pass
-
     def is_root(self):
-        return self._tags.__len__() == 1
+        return self._parent is None
 
     def is_my_child(self, node):
         return self.__eq__(node.get_parent())
 
-    def is_child_added(self, node):
-        is_added = False
-        for child in self._children:
-            is_added |= child.__eq__(node)
-        return is_added
+    def __hash__(self):
+        return hash(self.get_name_full())
 
-    def check_self_consistency(self):
-        is_consistent = True
-        self_level = self.get_node_level()
-        is_consistent &= self.get_name_full().split(Node.JOINER_NAME).__len__() == self_level
-        child_num_collection = []
-        for child in self._children:
-            is_consistent &= child.get_parent().get_name_full() == self.get_name_full()
-            is_consistent &= child.get_node_level() == self_level + 1
-            child_num_collection.__add__(child.get_tags()[-1])
-        child_num_collection.sort()
-        # TODO: find uniqueness for the child
-        return is_consistent
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
--- hierarchy/test/test_node.py
+++ hierarchy/test/test_node.py
@@ -3,75 +3,25 @@ from hierarchy.src.node import Node
 
 
 class TestNode(TestCase):
+    def test_init(self):
+        name = 'root'
+        root = Node(name)
+        self.assertIsInstance(root, Node)
+        pass
+
+    def test_is_root(self):
+        root = Node('root')
+        child = root.create_child('child')
+        self.assertTrue(root.is_root())
+        self.assertFalse(child.is_root())
+        pass
+
     def test_get_name_full(self):
-        nodes = []
-
-        root = Node("first generation - root", [0], None, [])
-        nodes.append(root)
-
-        child_1 = root.create_child("second generation - first child")
-        child_2 = root.create_child("second generation - second child")
-        nodes.append(child_1)
-        nodes.append(child_2)
-
-        grand_child_1 = child_1.create_child("third generation - first child")
-        grand_child_2 = child_1.create_child("third generation - second child")
-        grand_child_3 = child_2.create_child("third generation - third child")
-        grand_child_4 = child_1.create_child("third generation - fourth child")
-        nodes.append(grand_child_1)
-        nodes.append(grand_child_2)
-        nodes.append(grand_child_3)
-        nodes.append(grand_child_4)
-
-        grand_grand_child_1 = grand_child_3.create_child("fourth generation first child")
-        nodes.append(grand_grand_child_1)
-
-        for node in nodes:
-            print(node.get_name_full())
-        for node in nodes:
-            print(node.get_tags())
-
-        name_full = root.get_name_full()
-        name_full_split = name_full.split(Node.JOINER_NAME)
-        self.assertEquals(root.get_node_level(), name_full_split.__len__(), root.get_tags().__len__())
-
-        for node in [child_1, child_2]:
-            name_full = node.get_name_full()
-            name_full_split = name_full.split(Node.JOINER_NAME)
-            self.assertEquals(node.get_node_level(), name_full_split.__len__(), node.get_tags().__len__())
-
-        for node in [grand_child_1, grand_child_2, grand_child_3, grand_child_4]:
-            name_full = node.get_name_full()
-            name_full_split = name_full.split(Node.JOINER_NAME)
-            self.assertEquals(node.get_node_level(), name_full_split.__len__(), node.get_tags().__len__())
-
-        for node in [grand_grand_child_1]:
-            name_full = node.get_name_full()
-            name_full_split = name_full.split(Node.JOINER_NAME)
-            self.assertEquals(node.get_node_level(), name_full_split.__len__(), node.get_tags().__len__())
-        pass
-
-    def test_get_root(self):
-        root = Node("root", [0], None, [])
-        child = root.create_child("child")
-        grandchild = child.create_child("grandchild")
-
-        self.assertEqual(child.get_root().get_name_full(), grandchild.get_root().get_name_full())
-        self.assertEquals(1, child.get_root().get_node_level(), grandchild.get_root().get_node_level())
-        pass
-
-    def test_check_self_consistency(self):
-        root = Node("root", [0], None, [])
-        # a properly created child
-        child_consistent = root.create_child("child_consistent")
-        # grandchild indeed but set to child level
-        child_inconsistent = Node("child_inconsistent", [0, 2], child_consistent, [])
-        self.assertTrue(root.check_self_consistency())
-        self.assertTrue(child_consistent.check_self_consistency())
-        self.assertFalse(child_inconsistent.check_self_consistency())
-        pass
-
-    def test_is_my_child(self):
-        root = Node("root", [0], None, [])
-
-        pass
+        name_root = 'root'
+        name_child = 'child'
+        root = Node(name_root)
+        child = root.create_child(name_child)
+        self.assertEqual(name_root, root.get_name())
+        self.assertEqual(name_root, root.get_name_full())
+        self.assertEqual(name_child, child.get_name())
+        self.assertEqual(name_root + Node.JOINER_NAME + name_child, child.get_name_full())
